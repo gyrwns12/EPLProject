b5186d6ba0764177f4e5cf841d4fe6b3c7644c8b
gyrwns12 <83577443+gyrwns12@users.noreply.github.com>
2021-06-15 19:50:40 +0900

    Create MemberVo.java

---
+++ MemberVo.java
@@ -0,0 +1,34 @@
+
+
+public class MemberVo {
+	private String id;
+	private String password;
+	private String club;
+	
+	public MemberVo() {
+		
+	}
+	
+	public MemberVo(String id, String password) {
+		this.id = id;
+		this.password = password;
+	}
+	
+	public MemberVo(String id, String password, String club) {
+		this.id = id;
+		this.password = password;
+		this.club = club;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public String getClub() {
+		return club;
+	}
+}
